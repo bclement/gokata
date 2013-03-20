ca8bc466ab50ee59f88c39224262f41a0e2ae10f
Brian Clements <brian@clementscode.com>
2013-03-19 21:11:27 -0500

    Initial binary search impl, updated test

--- chop/chop.go
+++ chop/chop.go
@@ -1,5 +1,25 @@
 package chop
 
-func Chop(x int) int{
-    return x + 1;
+func Chop(x int, arr []int) int{
+    rval := -1
+    size := len(arr)
+    if size > 0{
+        mid := size / 2
+        diff := x - arr[mid]
+        switch {
+        case diff == 0:
+            return mid
+        case diff > 0:
+            ret := Chop(x, arr[mid+1:])
+            if ret == -1{
+                return ret
+            }else{
+                return ret + mid
+            }
+        case diff < 0:
+            return Chop(x, arr[:mid])
+        }
+    }
+    return rval;
 }
+
--- chop/chop_test.go
+++ chop/chop_test.go
@@ -3,8 +3,11 @@ package chop
 import "testing"
 
 func TestChop(t *testing.T){
-    const in, out = 1, 2
-    if x := Chop(in); x != out {
-        t.Errorf("chop is busted")
+    assert(t, 2, Chop(1, []int{-1,0,1,2,3}))
+}
+
+func assert(t *testing.T, expected int, result int){
+    if expected != result{
+        t.Errorf("Expected %d got %d", expected, result)
     }
 }
