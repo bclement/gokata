154ccd64ae8fc39644ba311bf738d2d5c9373e9f
Brian Clements <brian@clementscode.com>
2013-03-20 19:04:53 -0500

    Added bonus chop function

--- chop/chop.go
+++ chop/chop.go
@@ -5,7 +5,6 @@ Binary search that finds index of x in arr.
 Returns -1 if x is not n arr
 */
 func Chop(x int, arr []int) int{
-    rval := -1
     size := len(arr)
     if size > 0{
         mid := size / 2
@@ -24,6 +23,31 @@ func Chop(x int, arr []int) int{
             return Chop(x, arr[:mid])
         }
     }
-    return rval;
+    return -1;
 }
 
+/*
+Takes a value and an array that may contain that value.
+If the boolean return is true, the int is the index of the value.
+If the boolean return is false, the int is where the value would go.
+*/
+func Chop2(x int, arr []int) (int, bool){
+    size := len(arr)
+    if size > 0{
+        mid := size / 2
+        diff := x - arr[mid]
+        add := 0
+        switch {
+        case diff == 0:
+            return mid, true
+        case diff > 0:
+            arr = arr[mid+1:]
+            add += mid + 1
+        case diff < 0:
+            arr = arr[:mid]
+        }
+        ret, flag := Chop2(x, arr)
+        return ret + add, flag
+    }
+    return 0, false
+}
--- chop/chop_test.go
+++ chop/chop_test.go
@@ -30,3 +30,55 @@ func assert_equal(t *testing.T, expected int, result int){
         t.Errorf("Expected %d got %d", expected, result)
     }
 }
+
+func assert_equal2(t *testing.T, expIndex int, expFlag bool, resIndex int,
+resFlag bool){
+    if expIndex != resIndex || expFlag != resFlag{
+        t.Errorf("Expected %d,%t got %d,%t", expIndex,
+        expFlag, resIndex, resFlag)
+    }
+}
+
+func TestChop2(t *testing.T){
+    index, flag := Chop2(1, []int{-1,0,1,2,3})
+    assert_equal2(t, 2, true, index, flag)
+    index, flag = Chop2(3, []int{})
+    assert_equal2(t, 0, false, index,flag)
+    index, flag = Chop2(3, []int{1})
+    assert_equal2(t, 1, false, index,flag)
+    index, flag = Chop2(1, []int{1})
+    assert_equal2(t, 0, true, index,flag)
+    index, flag = Chop2(1, []int{1, 3, 5})
+    assert_equal2(t, 0, true, index,flag)
+    index, flag = Chop2(3, []int{1, 3, 5})
+    assert_equal2(t, 1, true, index,flag)
+    index, flag = Chop2(5, []int{1, 3, 5})
+    assert_equal2(t, 2, true, index,flag)
+    index, flag = Chop2(0, []int{1, 3, 5})
+    assert_equal2(t, 0, false, index,flag)
+    index, flag = Chop2(2, []int{1, 3, 5})
+    assert_equal2(t, 1, false, index,flag)
+    index, flag = Chop2(4, []int{1, 3, 5})
+    assert_equal2(t, 2, false, index,flag)
+    index, flag = Chop2(6, []int{1, 3, 5})
+    assert_equal2(t, 3, false, index,flag)
+    index, flag = Chop2(1, []int{1, 3, 5, 7})
+    assert_equal2(t, 0, true, index,flag)
+    index, flag = Chop2(3, []int{1, 3, 5, 7})
+    assert_equal2(t, 1, true,  index,flag)
+    index, flag = Chop2(5, []int{1, 3, 5, 7})
+    assert_equal2(t, 2, true,  index,flag)
+    index, flag = Chop2(7, []int{1, 3, 5, 7})
+    assert_equal2(t, 3, true,  index,flag)
+    index, flag = Chop2(0, []int{1, 3, 5, 7})
+    assert_equal2(t, 0, false, index,flag)
+    index, flag = Chop2(2, []int{1, 3, 5, 7})
+    assert_equal2(t, 1, false, index,flag)
+    index, flag = Chop2(4, []int{1, 3, 5, 7})
+    assert_equal2(t, 2, false, index,flag)
+    index, flag = Chop2(6, []int{1, 3, 5, 7})
+    assert_equal2(t, 3, false, index,flag)
+    index, flag = Chop2(8, []int{1, 3, 5, 7})
+    assert_equal2(t, 4, false, index,flag)
+}
+
