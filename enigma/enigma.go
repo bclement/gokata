9e302d51d7197eef0d7581af6a721296920b876d
Brian Clements <brian@clementscode.com>
2013-03-23 21:00:53 -0500

    added test file, simple structs

--- enigma/enigma.go
+++ enigma/enigma.go
@@ -1,5 +1,14 @@
 package enigma
 
+var I = []byte{1, 2, 3}
+
 type Rotor struct{
     arr []byte
+    step int
+    pos int
+}
+
+type Machine struct{
+    rotors []Rotor
+    reflector []byte
 }
---
+++ enigma/enigma_test.go
@@ -0,0 +1,9 @@
+package enigma
+
+import "testing"
+
+func TestEnig(t *testing.T){
+    if I[0] != 1{
+        t.Errorf("problem")
+    }
+}
