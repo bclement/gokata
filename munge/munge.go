86487581fc2efd88e02e411a8ef4398269aecb9a
Brian Clements <brian@clementscode.com>
2013-03-29 02:50:29 -0500

    Refactored diff function, added football test

--- munge/munge.go
+++ munge/munge.go
@@ -7,39 +7,60 @@ import "strconv"
 import "math"
 import "io"
 
-func SmallDiff(path string, re *regexp.Regexp) (string, error){
-    file, err := os.Open(path)
-    if ( err != nil){
-        return "", err
-    }
-    reader := bufio.NewReader(file)
-    var rval string
-    min := math.Inf(1)
-    for {
-        line, err := reader.ReadString('\n')
-        if err != nil{
-            if (err != io.EOF){
-                return "", err
-            }
-            break
-        }
-        match := re.FindStringSubmatch(line)
-        if len(match) < 4{
-            continue
-        }
-        floats := [2]float64{0.0,0.0}
-        for i := range floats{
-            val, err := strconv.ParseFloat(match[i+2], 64)
-            floats[i] = val
-            if err != nil{
-                return "", err
-            }
-        }
-        diff := math.Abs(floats[0]-floats[1])
-        if diff < min{
-            min = diff
-            rval = match[1]
-        }
-    }
-    return rval, nil
+/*
+Opens file at path and matches lines with re.
+Regexp should consist of three groups, a label and two values.
+Function returns label that has smallest diff between values.
+*/
+func SmallDiff(path string, re *regexp.Regexp) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	var rval string
+	min := math.Inf(1)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err != io.EOF {
+				return "", err
+			}
+			break
+		}
+		matched, label, diff, err := match(line, re)
+		if err != nil {
+			return "", err
+		}
+		if !matched {
+			continue
+		}
+		if diff < min {
+			min = diff
+			rval = label
+		}
+	}
+	return rval, nil
+}
+
+/*
+If line matches re, bool return is true, string is label and float is diff
+*/
+func match(line string, re *regexp.Regexp) (matched bool, label string,
+	diff float64, err error) {
+	match := re.FindStringSubmatch(line)
+	if matched = len(match) > 0; !matched {
+		return
+	}
+	label = match[1]
+	floats := [2]float64{0.0, 0.0}
+	for i := range floats {
+		floats[i], err = strconv.ParseFloat(match[i+2], 64)
+		if err != nil {
+			return
+		}
+	}
+	diff = math.Abs(floats[0] - floats[1])
+	return
 }
--- munge/munge_test.go
+++ munge/munge_test.go
@@ -3,13 +3,26 @@ package munge
 import "testing"
 import "regexp"
 
-func TestWeather(t *testing.T){
-    re, _ := regexp.Compile(`^\s+([0-9]+)[^0-9]+([0-9]+)[^0-9]+([0-9]+)`)
-    res, err := SmallDiff("weather.dat", re)
-    if err != nil{
-        t.Errorf("problem %s", err)
-    }
-    if res != "14"{
-        t.Errorf("result %s", res)
-    }
+func TestWeather(t *testing.T) {
+	pattern := `^\s+([0-9]+)[^0-9]+([0-9]+)[^0-9]+([0-9]+)`
+	run(t, "weather.dat", pattern, "14")
+}
+
+func TestFootball(t *testing.T) {
+	pattern := `^\s+\d+\.\s([a-zA-Z_]+).*\s(\d+)\s+-\s+(\d+)`
+	run(t, "football.dat", pattern, "Aston_Villa")
+}
+
+func run(t *testing.T, path string, pattern string, expected string) {
+	re, err := regexp.Compile(pattern)
+	if err != nil {
+		t.Errorf("pattern %s", err)
+	}
+	res, err := SmallDiff(path, re)
+	if err != nil {
+		t.Errorf("problem %s", err)
+	}
+	if res != expected {
+		t.Errorf("expected '%s' got '%s'", expected, res)
+	}
 }
