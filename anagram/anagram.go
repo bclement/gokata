f126a21e982bde8666ae35a7a270fba389997917
Brian Clements <brian@clementscode.com>
2013-04-12 22:32:00 -0500

    anagram kata

---
+++ anagram/anagram.go
@@ -0,0 +1,78 @@
+package anagram
+
+import "io"
+import "sort"
+import "bufio"
+import "strings"
+import "container/list"
+
+/*
+anagram collection results
+Dict is a map of ordered letters to lists of anagrams
+Longest is the key with the most letters
+Most is the key to the biggest list of anagrams
+*/
+type Results struct {
+    Dict map[string] *list.List
+    Longest string
+    Most string
+}
+
+/*
+Collect anagrams from readers, one per line
+*/
+func Collect(reader *bufio.Reader) (*Results, error) {
+    rval := &Results{make(map[string] *list.List),"", ""}
+    most := -1
+    for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err != io.EOF {
+				return nil, err
+			}
+			break
+		}
+        line = strings.TrimSpace(line)
+        sorted := getkey(line)
+        l, ok := rval.Dict[sorted]
+        if !ok {
+            l = list.New()
+            rval.Dict[sorted] = l
+        }
+        l.PushBack(line)
+        if l.Len() > most {
+            rval.Most = sorted
+            most = l.Len()
+        }
+    }
+    cull(rval)
+    return rval, nil
+}
+
+/*
+remove one-entry results
+finalize stats
+*/
+func cull(res *Results) {
+    longest := -1
+    for key, l := range res.Dict {
+        if l.Len() < 2 {
+            delete(res.Dict, key)
+            continue
+        }
+        if len(key) > longest {
+            res.Longest = key
+            longest = len(key)
+        }
+    }
+}
+
+/*
+get key from word
+*/
+func getkey(s string) string {
+    letters := strings.Split(s, "")
+    sort.Strings(letters)
+    return strings.Join(letters, "")
+}
+
---
+++ anagram/anagram_test.go
@@ -0,0 +1,56 @@
+package anagram
+
+import "os"
+import "fmt"
+import "net/http"
+import "container/list"
+import "bufio"
+import "testing"
+
+func TestSmall(t *testing.T) {
+	file, err := os.Open("small.txt")
+    assertErr(t, err)
+	defer file.Close()
+	reader := bufio.NewReader(file)
+    res, err := Collect(reader)
+    assertErr(t, err)
+    for _, l := range res.Dict {
+        logList(t, l)
+        t.Logf("\n")
+    }
+    assert(t, len(res.Dict) == 7, "anagram groups")
+    assert(t, len(res.Longest) == 7, "longest anagram")
+    t.Logf(res.Most)
+    l := res.Dict[res.Most]
+    assert(t, l.Len() == 4, fmt.Sprint("most anagrams", l))
+}
+
+func TestLarge(t *testing.T) {
+    response, err := http.Get("http://web.archive.org/web/20070704025714/http://pragmaticprogrammer.com/katadata/wordlist.txt")
+    assertErr(t, err)
+    reader := bufio.NewReader(response.Body)
+    res, err := Collect(reader)
+    assertErr(t, err)
+    assert(t, len(res.Dict) == 2006, fmt.Sprintf("anagram groups %d", len(res.Dict)))
+    assert(t, len(res.Longest) == 15, fmt.Sprintf("longest anagram %d", len(res.Longest)))
+    l := res.Dict[res.Most]
+    assert(t, l.Len() == 7, fmt.Sprintf("most anagrams %d", l.Len()))
+}
+
+func logList(t *testing.T, l *list.List) {
+    for e := l.Front(); e != nil; e = e.Next() {
+        t.Logf("%v", e.Value)
+    }
+}
+
+func assertErr(t *testing.T, err error) {
+    if err != nil {
+        t.Errorf("error %s", err)
+    }
+}
+
+func assert(t *testing.T, result bool, msg string) {
+    if !result {
+        t.Errorf(msg)
+    }
+}
