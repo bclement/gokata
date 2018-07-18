2b371c33532b8cfe0ba9d643d2fa35f8688639c1
Brian Clements <brian@clementscode.com>
2018-07-17 21:27:40 -0500

    moved packages into pkg directory

---
+++ pkg/heap/heap_test.go
@@ -0,0 +1,46 @@
+package heap
+
+import "testing"
+import "math/rand"
+
+func TestHeap(t *testing.T) {
+	h := MakeInt(2)
+	for i := 1; i < 200; i++ {
+		for n := 0; n < i; n++ {
+			h.Push(rand.Intn(100))
+		}
+		if l := h.Len(); l != i {
+			t.Errorf("busted %d, %d", l, h)
+		}
+		for last, _ := h.Pop().(int); h.Len() > 0; {
+			next, _ := h.Pop().(int)
+			if next < last {
+				t.Errorf("not sorted")
+			}
+			last = next
+		}
+	}
+}
+
+type Reverse struct {
+	IntStorage
+}
+
+func (r *Reverse) Less(i, j int) bool {
+	return r.IntStorage.Less(j, i)
+}
+
+func TestReverse(t *testing.T) {
+	v := Reverse{IntStorage{make([]int, 0, 10)}}
+	h := &Heap{&v, 2}
+	h.Push(2)
+	h.Push(3)
+	h.Push(1)
+	for last, _ := h.Pop().(int); h.Len() > 0; {
+		next, _ := h.Pop().(int)
+		if next > last {
+			t.Errorf("not reversed")
+		}
+		last = next
+	}
+}
